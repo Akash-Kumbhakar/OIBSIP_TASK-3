148c30876ed84da0eae3d59c09177b91e31a1904
Akash kumbhakar <samcodeaccess@gmail.com>
2022-04-25 01:47:18 +0530

    To-do app

--- main.js
+++ main.js
@@ -3,28 +3,27 @@ var input = document.querySelector('.input');
 const container = document.querySelector('.container');
 
 
-class item{
-    constructor(itemName){
-        //Create item div
+class item {
+    constructor(itemName) {
+
 
         this.createDiv(itemName);
 
     }
 
-    createDiv(itemName){
+    createDiv(itemName) {
 
         let input = document.createElement('input');
-        input.value= itemName;
+        input.value = itemName;
         input.disabled = true;
         input.classList.add('item_input')
-        input.type= "text";
+        input.type = "text";
 
         let itemBox = document.createElement('div');
         itemBox.classList.add('item')
 
         let editButton = document.createElement('button');
-        editButton.innerHTML = "EDIT";
-        editButton.classList.add('editButton');
+
 
         let removeButton = document.createElement('button');
         removeButton.innerHTML = "REMOVE";
@@ -36,37 +35,30 @@ class item{
         itemBox.appendChild(editButton);
         itemBox.appendChild(removeButton);
 
-     
-        editButton.addEventListener('click',() => this.edit(input));
 
-        removeButton.addEventListener('click',() => this.remove(itemBox));
+
+        removeButton.addEventListener('click', () => this.remove(itemBox));
 
     }
-
-    edit(input){
-
-        input.disabled =  !input.disabled;
-    }
-
-    remove(item){
+    remove(item) {
 
         container.removeChild(item);
     }
 
 }
 
-function check(){
+function check() {
 
-        if(input.value !=""){
-            new item(input.value);
-            input.value = "";
-        }
+    if (input.value != "") {
+        new item(input.value);
+        input.value = "";
+    }
 
 }
 
 addButton.addEventListener('click', check);
-window.addEventListener('keydown', (e)=>{
-    if(e.which == 13){
+window.addEventListener('keydown', (e) => {
+    if (e.which == 13) {
         check();
     }
 });
